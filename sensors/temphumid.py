ad5956eef6e99e748846def5d8424e8091d26cf1
ru9 <runisunde@gmail.com>
2019-07-29 22:08:16 +0200

    Formating temphumid
    Formated temphumid.py with degree symbol

--- sensors/temphumid.py
+++ sensors/temphumid.py
@@ -6,5 +6,5 @@ def run_dht():
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)
         global temp
         global humid
-        temp = '{0:0.1f} C'.format(temperature)
+        temp = '{0:0.1f} °C'.format(temperature)
         humid = '{0:0.1f} %'.format(humidity)
